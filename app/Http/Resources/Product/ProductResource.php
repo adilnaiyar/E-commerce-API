3d42c8d84620aea66f45cda0b5c22139426bdf92
Adil Naiyar <adil.naiyar@gail.com>
2020-04-30 20:52:22 +0530

    modified product detail

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -16,11 +16,18 @@ class ProductResource extends JsonResource
     {
         return [
 
-            'name' => $this->name,
-            'description' => $this->detail,
-            'price' => $this->price,
-            'stock' => $this->stock,
-            'discount' => $this->discount,
+            'name'          => $this->name,
+            'description'   => $this->detail,
+            'price'         => $this->price,
+            'stock'         => $this->stock == 0 ? "Out of Stock" : $this->stock,
+            'discount'      => $this->discount,
+            'totalPrice'    => round((1-($this->discount/100))*$this->price, 2),
+            'rating'        => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(), 2) : "No rating yet",
+
+            'href' => [
+
+                'link' => route('reviews.index', $this->id)
+            ]
         ];
     }
 }
